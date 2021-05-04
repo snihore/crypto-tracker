74205e3f296b737c9308859bd2090b66b7919bb3
snihore <souravnihore1998@gmail.com>
2021-05-04 19:28:18 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const URL = `https://vast-fjord-21675.herokuapp.com/`;
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      api_data: [],
+      isLoaded: false
+    }
+  }
+
+
+  
+  componentDidMount(){
+
+    try{
+
+      fetch('/api_data')
+      .then(data=>data.json())
+      .then(json => {
+        // console.log(json);
+        this.setState({
+          isLoaded: true,
+          api_data: json
+        });
+      });
+
+    }catch(err){
+
+    }
+
+  }
+
+  render(){
+
+    var { isLoaded, api_data } = this.state;
+
+    if(!isLoaded){
+      return <div>Loading ...</div>;
+    }
+    return (
+
+      <div className="App">
+        <ul style={{listStyleType:"none"}}>
+          {
+            api_data.map(data =>(
+                <li key={data.buyAt}>
+                  <strong>User Name: </strong><span>{data.userName}</span><br />
+                  <strong>{data.time}</strong><br /><br />
+                  <strong>Coin Name: </strong><span>{data.name}</span><br />
+                  <strong>Buy At: </strong><span>{data.buyAt} </span>
+                  <strong>Total Buy At: </strong><span>{data.totalBuyAt} </span>
+                  <strong>Quantity: </strong><span>{data.quantity}</span><br />
+                  <strong>LTP: </strong><span>{data.last} </span>
+                  <strong>Total Now: </strong><span>{data.totalNow} </span><br />
+                  <strong>P&amp;L: </strong><span>{data.profit_and_loss} </span>
+                  <strong>ROI: </strong><span>{data.profit_and_loss_per}% </span><br />
+                  <hr />
+                </li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
